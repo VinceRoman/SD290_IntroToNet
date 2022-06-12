26cd72ba94cbdd94c49a482a70330d91ae4c5047
Vince Roman <vince.aragorn.roman@gmail.com>
2022-06-12 16:59:21 -0500

    add isPalindrome function

--- IntroToNet/IntroToNet/Program.cs
+++ IntroToNet/IntroToNet/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(isPalindrome("racecarracecar"));
+
+bool isPalindrome(string inputWord)
+{
+    string loweredInput = inputWord.ToLower();//easier comparison if everything is lowercase.
+    char[] charArray = loweredInput.ToCharArray();
+    int reverseIndex = charArray.Length - 1;
+    bool isValid = true;
+
+    for(int i = 0; i < charArray.Length; i++)//barrier to prevent things like spaces or symbols
+    {
+        if(Char.IsLetterOrDigit(charArray[i]) == false)
+        {
+            Console.WriteLine("You mustn't add spaces or symbols.");
+            isValid = false;
+        }
+    }
+
+    for (int index = 0; index < charArray.Length; index++)
+    {
+        if (charArray[index] != charArray[reverseIndex])
+        {
+            isValid = false;
+        }
+        reverseIndex--;
+    }
+
+    if (isValid)
+    {
+        
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
